c93c3d91dae92a6dda30aafb647e07c5718e8945
Nan Davenport <nannigan@nannigans.com>
2015-04-22 14:59:52 -0700

    implemented pick gender

--- js/madLibs.js
+++ js/madLibs.js
@@ -11,21 +11,7 @@ angular.module('madLibs', [])
 	$scope.language =  "";
 	$scope.web =  "";
 	$scope.gender ='female' || 'male';
-	//does default to female in initial dom state
-	// jQuery("#gen").on ('click', function(){
-	// var foo = jQuery("#gen");
-	
-	// 	if(foo.prop('checked'))
-	// 		{
-	// 			gender = 'male';
-	// 			console.log('yep checked');
-	// 			//does log, but doesn not change 
-	// 			//ng-if 
-	// 		}else {
-	// 			gender = 'female';
-	// 			// for unchecking
-	// 		}
-	// 	});
+
 	
 	}
 );
